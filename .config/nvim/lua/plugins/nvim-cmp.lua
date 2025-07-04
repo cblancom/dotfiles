32d2cbb029a435c66af7aa930386a9540aee794f
Cristian Blanco <cristian.blanco@utp.edu.co>
2025-07-04 16:08:30 -0500

    Revert "Copilot and Copilot chat is now usable in the Nvim configuration"
    This reverts commit 23b6c05176ad18a12ffd6e8f1f8bbc937a55af46.

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -36,7 +36,6 @@ return {
             }),
             -- sources for autocompletion
             sources = cmp.config.sources({
-                { name = "copilot" },
                 { name = "nvim_lsp" },
                 { name = "luasnip" }, -- snippets
                 { name = "buffer" }, -- text within current buffer
