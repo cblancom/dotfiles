1cb40aa33fc4ed33acad313271ec4f006ae1497c
Cristian Blanco <cristian.blanco@utp.edu.co>
2025-07-18 00:17:40 -0500

    chore(config): update Neovim config for scrolloff, LSP, and Hardtime
    - Added `scrolloff` option for better cursor context during scrolling.
    - Switched LSP configuration to use Neovim's built-in LSP client instead of a plugin.
    - Modified Hardtime plugin settings to prevent it from starting on Neovim startup.

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -17,10 +17,12 @@ vim.cmd.colorscheme = "kanagawa-wave"
 vim.opt.clipboard = "unnamedplus"
 
 -- spell schecker
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'tex',
-  callback = function()
-    vim.opt_local.spell = true
-    vim.opt_local.spelllang = 'es'
-  end,
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "tex",
+	callback = function()
+		vim.opt_local.spell = true
+		vim.opt_local.spelllang = "es"
+	end,
 })
+
+vim.o.scrolloff = 999
--- .config/nvim/lua/plugins/hardtime.lua
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -1,8 +1,10 @@
 return {
-{
-   "m4xshen/hardtime.nvim",
-   lazy = false,
-   dependencies = { "MunifTanjim/nui.nvim" },
-   opts = {},
-},
+	{
+		"m4xshen/hardtime.nvim",
+		lazy = true,
+		dependencies = { "MunifTanjim/nui.nvim" },
+		opts = {},
+		-- Esto define un comando para cargar el plugin manualmente
+		cmd = { "HardtimeEnable", "HardtimeDisable" },
+	},
 }
